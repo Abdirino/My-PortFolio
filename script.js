b4698ffc4aaa9df44f4a031ab0540f9cc014769a
Abdirino <abdirinoahash@gmail.com>
2022-11-24 08:18:05 +0300

    auto-complete text start

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el))
 // end on-scroll animation
+
+// auto-complete text
+
+// end auto-complete text
